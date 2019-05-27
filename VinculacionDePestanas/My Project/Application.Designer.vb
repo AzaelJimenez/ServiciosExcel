70c21b92d5020d10223befaca8a2854aeb52fedf
PaauRF <paaurodriiguez@gmail.com>
2019-05-27 17:35:44 -0500

    Referencias

--- VinculacionDePestanas/My Project/Application.Designer.vb
+++ VinculacionDePestanas/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VinculacionDePestanas.FrmMenu
+            Me.MainForm = Global.VinculacionDePestanas.ImprmirInforme
         End Sub
     End Class
 End Namespace
